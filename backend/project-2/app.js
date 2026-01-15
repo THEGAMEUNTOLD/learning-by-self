40d9f81b7e12c1434b6a6cd002da9739cfb6d79a
bharat <bharatchoutharyb@gmail.com>
2026-01-15 12:23:29 +0530

    done

---
+++ backend/project-2/app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//  Correct model import
+const User = require('./models/User');
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/userdb')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// View engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/read', async (req, res) => {
+    const users = await User.find();
+    res.render('read', { users });
+});
+
+// ✅ CREATE USER (POST)
+app.post('/create', async (req, res) => {
+    const { name, email, image } = req.body;
+
+    await User.create({
+        name,
+        email,
+        image
+    });
+
+    res.redirect('/read');
+});
+
+app.get('/read', async (req, res) => {
+    const users = await User.find();
+    res.render('read', { users });
+});
+
+app.get('/delete/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect('/read');
+});
+
+app.get('/edit/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    res.render('edit', { user });
+});
+
+app.post('/update/:id', async (req, res) => {
+    const { name, email, image } = req.body;
+
+    await User.findByIdAndUpdate(req.params.id, {
+        name,
+        email,
+        image
+    });
+
+    res.redirect('/read');
+});
+
+
+
+// Server
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
